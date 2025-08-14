903894bd69a44ce55fae103e6aa2c60b05bfb9db
abhishek-akp <abhishekkumarpandey2803@gmail.com>
2025-08-14 08:53:03 -0400

    fix build

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,11 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   output: 'export',
-  /* config options here */
-};
+  trailingSlash: true,
+  images: {
+    unoptimized: true
+  }
+}
 
-
+export default nextConfig
