b00e82bc68d744a16a7f1c44b09f1be0500196ba
abhishek-akp <abhishekkumarpandey2803@gmail.com>
2025-08-14 09:13:00 -0400

    fix b

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,7 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  output: 'export',
-  distDir: 'out',
   trailingSlash: true,
-  skipTrailingSlashRedirect: true,
   images: {
     unoptimized: true
   }
